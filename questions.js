d38296726b2881187ca5f9ce7f486e34ac283e1b
Enid <enid.soto@gmail.com>
2020-01-21 20:29:09 -0500

    fixed js to make questions display properly

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 var questions = [
     {
         title: "What does CSS stand for?",
-        choices: ["Cascading Style Sheets","Computing Style Sheets","Creative Styles Services","Color Style Services"],
+        choices: ["Cascading Style Sheets","Computing Style Sheets","Creative Style Services","Color Style Services"],
         answer: "Cascading Style Sheets"
     },
     {
@@ -25,63 +25,4 @@ var questions = [
         answer: "#main"
     }
 ];
-
-var score = 0;
-var timer = 0;
-var startBtn = document.getElementById("startQuiz");
-
-var highScores = [
-    {
-        initials: "",
-        finalScore: 0
-    }
-];
-
-startBtn.addEventListener("click", function(){
-
-     // to hide the jumbotron
-
-    var jumbotron = document.getElementById("jumbotron");
-    jumbotron.classList.add("d-none");
-
-    var questionBox = document.getElementById("questionsBox");
-    questionBox.classList.remove("d-none");
-    questionBox.setAttribute("index", "0");
-
-    var header = document.getElementById("quiz-header");
-    header.classList.remove("d-none");
-
-    var questionsCounter = 0;
-
-    questionsControl(questionsCounter);
-
-});
-
-function questionsControl(q) {
-
-    var questionHeader = document.getElementById("questionTitle");
-    questionHeader.innerText = questions[q].title;
-    var currentQuestion = questions[q];
-    var qNumber = document.getElementById("qNumber");
-    qNumber.innerText = "Question "+(q+1);
-
-    // the following loop displays the current question and answer list
-    for (var i=0; i < questions[q].choices.length; i++) {
-        var listItem = document.createElement('li');
-        var answer = currentQuestion.choices[i];
-        listItem.innerText = answer;
-        listItem.setAttribute("id",i);
-        document.getElementById("answers").appendChild(listItem);
-    };
-
-    //trying to set up the click event - TESTING THIS CURRENTLY
-    var clicked = document.getElementById('0');
-
-    clicked.addEventListener("click", function(event) {
-        if(clicked) {
-            alert("hello");
-        }
-    });
-
-};
 
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-/* //var score = 0;
-//var timer = 0;
+var score = 0;
+var timer = 0;
 var startBtn = document.getElementById("startQuiz");
-console.log(startBtn);
+
 var highScores = [
     {
         initials: "",
@@ -10,8 +10,83 @@ var highScores = [
 ];
 
 startBtn.addEventListener("click", function(){
-    //console.log(questions);
-    alert("hello");
+
+    var jumbotron = document.getElementById("jumbotron");
+    jumbotron.classList.add("d-none"); // hides the jumbotron
+
+    var header = document.getElementById("quiz-header");
+    header.classList.remove("d-none"); // shows the timer and high scores
+
+    var questionsCounter = 0; // Initializes the counter to get the first question in the array
+    var numberOfQuestions = questions.length;
+
+    populateFirstQuestion();
+
 });
 
- */
+var clicked = document.getElementById('answers');
+clicked.addEventListener("click", function(event) {
+
+    var CurrentIndex = document.getElementById("questionsBox").getAttribute("index");
+    //console.log(CurrentIndex);
+    var selectedAnswer = event.target.innerText;
+    var correctAnswer = questions[CurrentIndex].answer;
+    //console.log(correctAnswer);
+    if(selectedAnswer == correctAnswer) {
+        CurrentIndex++;
+        generateQuestion(CurrentIndex);
+        //alert("display next question");
+    } else {
+        alert("call penalties function");
+    }
+});
+
+function generateQuestion(q) {
+
+    var questionBox = document.getElementById("questionsBox");
+    questionBox.setAttribute("index", q);
+
+    var questionHeader = document.getElementById("questionTitle");
+    questionHeader.innerText = questions[q].title;
+    var currentQuestion = questions[q];
+    var qNumber = document.getElementById("qNumber");
+    qNumber.innerText = "Question "+(q+1);
+
+    // removes previusly appended li items
+    var answers = document.getElementById("answers");
+    while (answers.hasChildNodes()) {  
+        answers.removeChild(answers.firstChild);
+      }
+
+    // the following loop displays the current question and answer list
+    for (var i=0; i < questions[q].choices.length; i++) {
+        var listItem = document.createElement('li');
+        var answer = currentQuestion.choices[i];
+        listItem.innerText = answer;
+        listItem.setAttribute("id",i);
+        document.getElementById("answers").appendChild(listItem);
+    };
+
+}
+
+function populateFirstQuestion() {
+    var questionBox = document.getElementById("questionsBox");
+    questionBox.classList.remove("d-none");
+    questionBox.setAttribute("index", 0);
+
+    var questionHeader = document.getElementById("questionTitle");
+    questionHeader.innerText = questions[0].title;
+    var currentQuestion = questions[0];
+    var qNumber = document.getElementById("qNumber");
+    qNumber.innerText = "Question "+(0+1);
+
+    // the following loop displays the current question and answer list
+    for (var i=0; i < questions[0].choices.length; i++) {
+        var listItem = document.createElement('li');
+        var answer = currentQuestion.choices[i];
+        listItem.innerText = answer;
+        listItem.setAttribute("id",i);
+        document.getElementById("answers").appendChild(listItem);
+    };
+}
+
