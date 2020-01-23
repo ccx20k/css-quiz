f7c8f32f32411c783ea695ef8f79a9b78f1cc3bf
enma1009 <enid.soto@gmail.com>
2020-01-23 14:17:46 -0500

    worked in the timer functionality

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,31 @@
+// GLOBAL VARIABLES
 var score = 0;
-var timer = 0;
 var startBtn = document.getElementById("startQuiz");
-
 var highScores = [
     {
         initials: "",
         finalScore: 0
     }
 ];
-
 var questionBox = document.getElementById("questionsBox");
+var correctAnswerAlert = document.querySelector(".alert-success");
+var wrongAnswerAlert = document.querySelector(".alert-danger");
 
+// EVENT LISTENERS
 startBtn.addEventListener("click", function(){
-
-    setQuizTimer(); //  initializes the countdown
 
     var jumbotron = document.getElementById("jumbotron");
     jumbotron.classList.add("d-none"); // hides the jumbotron
 
     var header = document.getElementById("quiz-header");
     header.classList.remove("d-none"); // shows the timer and high scores
-
     questionBox.classList.remove("d-none"); // shows the div containing the question
+    
     var questionsCounter = 0; // Initializes the counter to get the first question in the array
-    var numberOfQuestions = questions.length;
-
     generateQuestion(questionsCounter); // displays the first question
+
+    var timer = quizTimer(); //  initializes the countdown
+    console.log(typeof timer);
 
 });
 
@@ -33,23 +33,42 @@ var clicked = document.getElementById('answers');
 clicked.addEventListener("click", function(event) {
 
     var CurrentIndex = document.getElementById("questionsBox").getAttribute("index");
-    //console.log(CurrentIndex);
+    var numberOfQuestions = questions.length;
     var selectedAnswer = event.target.innerText;
     var correctAnswer = questions[CurrentIndex].answer;
-    //console.log(correctAnswer);
-    if(selectedAnswer == correctAnswer) {
-
-        // TO DO - place an if statement here to check if the user is in the last question 
+    
+    if(selectedAnswer == correctAnswer) { 
         score +=100;
-        CurrentIndex++;
         console.log(score);
-        generateQuestion(CurrentIndex);
-        //alert("display next question");
+        CurrentIndex++;
+        
+        if (CurrentIndex < numberOfQuestions) {
+            correctAnswerAlert.classList.remove("d-none");
+            setTimeout(function() { 
+                correctAnswerAlert.classList.add("d-none");
+                generateQuestion(CurrentIndex);
+            }, 500);
+            //generateQuestion(CurrentIndex);
+        }
+    
     } else {
-        alert("call penalties function");
+        if (CurrentIndex == numberOfQuestions-1) {
+            endQuiz();
+        } else {
+            wrongAnswerAlert.classList.remove("d-none");
+            score -=10;
+            console.log(score);
+            CurrentIndex++;
+            setTimeout(function() { 
+                wrongAnswerAlert.classList.add("d-none");
+                generateQuestion(CurrentIndex);
+            }, 500);
+            //generateQuestion(CurrentIndex);
+        } 
     }
 });
 
+// FUNCTIONS
 function generateQuestion(q) {
 
     questionBox.setAttribute("index", q);
@@ -77,7 +96,7 @@ function generateQuestion(q) {
 
 };
  
-function setQuizTimer(){
+function quizTimer() {
 
     var quizRunTime = 90;
     var timerDisplay = document.getElementById("timer");
@@ -88,8 +107,17 @@ function setQuizTimer(){
 
         if(quizRunTime === 0) {
             clearInterval(timerInterval);
-            alert("Time ran out!");
-          }
+            endQuiz();
+          } 
       
     }, 1000);
+
+     return timerInterval;
 };
+
+function endQuiz() {
+    var endingDiv = document.getElementById("endOfQuiz");
+    var questionBox = document.getElementById("questionsBox");
+    questionBox.classList.add("d-none");
+    endingDiv.classList.remove("d-none"); 
+}
